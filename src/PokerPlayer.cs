01dd9178de4d899cd1a00bef34ac75ebbbbb25b0
Benny.Kammer <Benny.Kammer@digitecgalaxus.ch>
2021-03-10 09:57:50 +0100

    First Commit

--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -4,12 +4,14 @@ namespace Nancy.Simple
 {
 	public static class PokerPlayer
 	{
-		public static readonly string VERSION = "Default C# folding player";
+		public static readonly string VERSION = "Stone Cold Nuts";
 
 		public static int BetRequest(JObject gameState)
 		{
-			//TODO: Use this method to return the value You want to bet
-			return 0;
+            //TODO: Use this method to return the value You want to bet
+            var game_state = gameState.ToObject<GameState>();
+            var player = game_state.Players[game_state.In_Action];
+            return player.Stack;
 		}
 
 		public static void ShowDown(JObject gameState)
@@ -17,5 +19,44 @@ namespace Nancy.Simple
 			//TODO: Use this method to showdown
 		}
 	}
+    public class GameState
+    {
+        public Player[] Players;
+        public string Tournament_Id;
+        public int Round;
+        public int Small_Blind;
+        public int Current_Buy_In;
+        public int Pot;
+        public int Minimum_Raise;
+        public int Bet_Index;
+        public int Orbits;
+        public int Dealer;
+        public Card[] Community_Cards;
+        public int In_Action;
+    }
+
+    public class Player
+    {
+        public int Id;
+        public string Name;
+        public int Stack;
+        public PlayerStatus Status;
+        public string Version;
+        public int Bet;
+        public Card[] Hole_Cards;
+    }
+
+    public enum PlayerStatus
+    {
+        active,
+        folded,
+        @out
+    }
+
+    public class Card
+    {
+        public string Rank;
+        public string Suit;
+    }
 }
 
