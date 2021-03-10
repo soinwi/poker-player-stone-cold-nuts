0cffc762e4c6117a7f7e7303d71c2f4d98446e84
soiniwi <ds@soinwi.ch>
2021-03-10 10:53:41 +0100

    numberofremaining players

---
+++ src/Helpers.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace Nancy.Simple
+{
+    public static class Helpers
+    {
+        public static int GetNumberOfRemainingPlayers(this GameState gameState)
+        {
+            return gameState.Players.Where(p => p.Id != gameState.Players[gameState.In_Action].Id)
+                .Count(p => p.Status == PlayerStatus.active);
+        }
+    }
+}
